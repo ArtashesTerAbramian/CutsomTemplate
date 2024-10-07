fa2d651f8b70fe9ed29e90cc8311cc1b81897455
Artashes Ter-Abrahamyan <60017912+ArtashesTerAbramian@users.noreply.github.com>
2024-10-07 22:32:19 +0400

    fixes for filename, net version, IcontextModification

--- Template.BLL/ServiceExtension.cs
+++ Template.BLL/ServiceExtension.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Template.BLL.Services.ContextModificatorService;
 using Template.BLL.Services.ErrorService;
+using Template.DAL;
 
 namespace Template.BLL;
 public static class ServiceExtension 
 {
     public static IServiceCollection AddWebServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddScoped<IContextModificatorService, ContextModificatorService>();
         services.AddScoped<IErrorService, ErrorService>();
 
         return services;
---
+++ Template.BLL/Services/ContextModificatorService/ContextModificatorService.cs
@@ -0,0 +1,8 @@
+﻿using Template.DAL;
+
+namespace Template.BLL.Services.ContextModificatorService;
+
+public class ContextModificatorService : IContextModificatorService
+{
+    public bool IsGlobalQueryFiltersEnable { get; } = true;
+}
